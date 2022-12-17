fb906deb58700fd62fd0c663e4c41787f3f64735
unknown <muqymo@gmail.com>
2022-12-17 17:51:27 +0800

    start to make the login and signup page

--- js/nav.js
+++ js/nav.js
@@ -5,12 +5,12 @@ function writeNavigation() {
     document.write('  <div class="logo"><a href="#">CollectWay</a></div>');
     document.write('    <ul>');
     document.write('      <label class="btn cancel" for="check"><i class="fas fa-times"></i></label>');
-    document.write('      <li><a href="#">Home</a></li>');
+    document.write('      <li><a href="index.html">Home</a></li>');
     document.write('      <li><a href="#">About</a></li>');
     document.write('      <li><a href="#">Services</a></li>');
     document.write('      <li><a href="#">Contact</a></li>');
-    document.write('     <li><a href="#">Feedback</a></li>');
-    document.write('      <li><a href="#">Login</a></li>');
+    document.write('     <li><a href="#">Pricing</a></li>');
+    document.write('      <li><a href="login-signup.html">Login</a></li>');
     document.write('    </ul>');
     document.write('    <label for="check" class="btn bars"><i class="fas fa-bars"></i></label>');
     document.write('  </div>');
